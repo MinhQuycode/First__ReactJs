9dce6db4537c9c45df4d29744061750b4271102d
MinhQuycode <phamminhquy0403@gmail.com>
2020-12-06 01:07:18 +0700

    reactjs

---
+++ reactjs/src/component/BaiTapLayout/BaiTapLayOut.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import BTCarousel from './BTCarousel'
+import BTFooter from './BTFooter'
+import BTHeader from './BTHeader'
+import BTSmartPhone from './BTSmartPhone'
+
+
+export default class BaiTapLayOut extends Component {
+    render() {
+        return (
+            <div>
+                <BTHeader/>
+                <BTCarousel/>
+                <BTSmartPhone/>
+                <BTFooter/>
+            </div>
+        )
+    }
+}
+
+
+
